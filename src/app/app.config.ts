30d1d3d47262bf3b2e8545fbfeaed616711765ed
DevJaGz <jgomezpersonal7@gmail.com>
2024-01-13 02:52:49 +0100

    build: remove fountsource poppins

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,5 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import '@fontsource/poppins';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
